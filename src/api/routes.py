8f71ecef5456c0698bd83b0ac814cb6df0cabf17
Cristopher Ruiz <cris-nando01@hotmail.com>
2021-04-28 03:16:48 +0000

    subo el el fogot pass

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,9 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
@@ -8,11 +11,31 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+#@api.route('/hello', methods=['POST', 'GET'])
+#def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend"
-    }
+    #response_body = {
+        #"message": "Hello! I'm a message that came from the backend"
+    #}
 
-    return jsonify(response_body), 200
+    #return jsonify(response_body), 200
+
+
+@api.route("/forgot_pass", methods=["POST"])
+def forgot_pass():
+    email=request.json.get("email", None)
+    password=request.json.get("password", None)
+
+message = Mail(
+    from_email='finanestu19@gmail.com',
+    to_emails=email,
+    subject='Recuperacion de contraseña',
+    html_content='<strong>Su contraseña actual es {password} </strong>')
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
