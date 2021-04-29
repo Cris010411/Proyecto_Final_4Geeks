f27c01de3fd5e58035bd1be4e2cbb24fe385e04e
Esther Rosales <estherrosales.erb@gmail.com>
2021-04-29 03:52:40 +0000

    Se creo la base de datos y los campos

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,11 +8,20 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/createUser', methods=['POST'])
+def create_User():
+    id=request.json.get("id",None)
+    email=request.json.get("email",None)
+    password=request.json.get("password",None)
+    age=request.json.get("age",None)
+    gender=request.json.get("gender",None)
+    country=request.json.get("country",None)
+    mascot=request.json.get("mascot",None)
+    is_active=request.json.get("is_active",None)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend"
-    }
+    user=User(id=id, email=email, password=password, age=age, gender=gender, country=country, 
+    mascot=mascot, is_active=is_active)
+    db.session.add(user)
+    db.session.commit()
 
     return jsonify(response_body), 200
