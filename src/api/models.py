80f4ce313a78277274b52d108a65a3f044793fd6
Cristopher Ruiz <cris-nando01@hotmail.com>
2021-04-29 01:06:42 +0000

    se creo la tabla de preguntas

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,22 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    test_log = db.Column(db.String(120), unique=True, nullable=False)
+    frase = db.Column(db.String(80), unique=False, nullable=False)
+    option = db.Column(db.String(80), unique=False, nullable=False)
+    type_test = db.Column(db.String(80), unique=False, nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #user = db.relationship("User") esto va para el usuario
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<test_log %r>' % self.test_log
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "test_log": self.test_log,
+            "frase": self.frase,
+            "option": self.option,
+            "type_test": self.type_test,
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
@@ -16,3 +17,21 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/test", methods=["GET"])
+def Test():
+    test_pri=Test.query.all()
+    test_pri = list(map(lambda x: x.serialize(), test_pri))
+    return jsonify({"results":test_pri})
+
+@api.route("/test", methods=["POST"])
+def Test_agregar():
+    test_log=request.json.get("test_log", None)
+    frase=request.json.get("frase",None)
+    option=request.json.get("option",None)
+    type_test=request.json.get("type_test",None)
+    test=Test(test_pri=test_pri, frase=frase, option=option, test_log=test_log)
+    db.session.add(test)
+    db.session.commit()
+    #user=json.loads(name, color_ojos, color_cabello,gender)
+    return jsonify({"people":"ok"})
