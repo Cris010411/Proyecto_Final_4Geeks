a6de0e9d2b670ea8714ecd4fdf41b0d57900ef0b
Cristopher Ruiz <cris-nando01@hotmail.com>
2021-04-30 04:10:16 +0000

    se creo base de datos del user y test

--- src/api/models.py
+++ src/api/models.py
@@ -2,12 +2,34 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     test_log = db.Column(db.String(120), unique=True, nullable=False)
     frase = db.Column(db.String(80), unique=False, nullable=False)
     option = db.Column(db.String(80), unique=False, nullable=False)
     type_test = db.Column(db.String(80), unique=False, nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #user = db.relationship("User") esto va para el usuario
+
+    def __repr__(self):
+        return '<test_log %r>' % self.test_log
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "test_log": self.test_log,
+            "frase": self.frase,
+            "option": self.option,
+            "type_test": self.type_test,
+            # do not serialize the password, its a security breach
+        }
+
+class UseTest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User")
+    id_test = db.Column(db.Integer, db.ForeignKey('test.id'))
+    test = db.relationship("Test")
     #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     #user = db.relationship("User") esto va para el usuario
 
