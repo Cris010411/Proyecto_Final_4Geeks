55e1cbd4dfd1ba7be211b120241a51b665ccc0ab
Esther Rosales <estherrosales.erb@gmail.com>
2021-04-30 05:40:55 +0000

    se creo tabla de usuario con name,password,birthday,gender,email.

--- src/api/models.py
+++ src/api/models.py
@@ -5,13 +5,12 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__= 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(180), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    age = db.Column(db.Integer, unique=False, nullable=False)
+    birthday = db.Column(db.String(120), unique=False, nullable=False)
     gender = db.Column(db.String(120), unique=False, nullable=False)
-    country = db.Column(db.String(120), unique=False, nullable=False)
-    mascot = db.Column(db.String(120), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -21,4 +20,4 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
-        }
+        }	
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,27 +1,27 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+
 api = Blueprint('api', __name__)
 
-
+   
 @api.route('/createUser', methods=['POST'])
 def create_User():
     id=request.json.get("id",None)
-    email=request.json.get("email",None)
+    name=request.json.get("name",None)
     password=request.json.get("password",None)
-    age=request.json.get("age",None)
+    birthday=request.json.get("birthday",None)
     gender=request.json.get("gender",None)
-    country=request.json.get("country",None)
-    mascot=request.json.get("mascot",None)
-    is_active=request.json.get("is_active",None)
+    email=request.json.get("email",None)
+       
 
-    user=User(id=id, email=email, password=password, age=age, gender=gender, country=country, 
-    mascot=mascot, is_active=is_active)
+    user=User(id=id, name=name, password=password, birthday=birthday, gender=gender, email=email)
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({"user"}) 
+    return jsonify({"user":"ok"}) 
